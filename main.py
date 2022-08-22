e61a31863b0d8ec09aafe9fd6119305ca564483c
Divyesh Dhanwani <divyeshkdhanwani@gmail.com>
2022-08-22 20:29:27 +0530

    adding code in main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+from functools import reduce
+
+def add_num(x,y):
+    return x+y
 
 # reference ...https://docs.python.org/3/library/__main__.html?highlight=main#module-__main__
 if __name__ == "__main__":
+    a = [10,200,30,10]
+    print(reduce(add_num,a))
     print("Hello Git")
